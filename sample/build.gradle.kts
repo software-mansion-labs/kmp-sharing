b9a2034e5c7ae25b38ad0da4fe2947da1072b7af
Artur Gęsiarz <126520293+arturgesiarz@users.noreply.github.com>
2025-10-03 14:21:27 +0200

    Dependencies update

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.composeMultiplatform)
-    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetBrains.compose)
+    alias(libs.plugins.jetBrains.kotlin.multiplatform)
+    alias(libs.plugins.jetBrains.kotlin.plugin.compose)
 }
 
 kotlin {
@@ -28,30 +28,28 @@ kotlin {
     sourceSets {
         androidMain.dependencies {
             implementation(compose.preview)
-            implementation(libs.androidx.activity.compose)
+            implementation(libs.androidX.activity.compose)
         }
         commonMain.dependencies {
-            implementation(compose.runtime)
-            implementation(compose.foundation)
-            implementation(compose.material3)
-            implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
-            implementation(libs.androidx.lifecycle.viewmodelCompose)
-            implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(compose.foundation)
+            implementation(compose.material3)
+            implementation(compose.runtime)
+            implementation(compose.ui)
+            implementation(libs.jetBrains.androidX.lifecycle.runtimeCompose)
+            implementation(libs.jetBrains.androidX.lifecycle.viewmodelCompose)
         }
-        commonTest.dependencies {
-            implementation(libs.kotlin.test)
-        }
+        commonTest.dependencies { implementation(libs.jetBrains.kotlin.test) }
     }
 }
 
 android {
-    namespace = "com.swmansion.kmpsharing"
+    namespace = "com.swmansion.kmpsharing.sample"
     compileSdk = libs.versions.android.compileSdk.get().toInt()
 
     defaultConfig {
-        applicationId = "com.swmansion.kmpsharing"
+        applicationId = "com.swmansion.kmpsharing.sample"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = 1
